a09854d1d0a1ae5ff9f6d8c595ced4a2a8c357c8
Ilya <daduuuuu79@gmail.com>
2024-09-14 11:26:39 +0300

    commit 1 and  11 exixais

--- app/src/main/java/com/example/mobiledev/Main.kt
+++ app/src/main/java/com/example/mobiledev/Main.kt
@@ -1,5 +1,6 @@
 package com.example.mobiledev
-
+import kotlin.math.PI
+//_________0*_________
 /*fun main() {
 
   println("Введите первое число")
@@ -13,7 +14,10 @@ var summ:Int=sum1+sum2
     println("sum ="+" " + summ+"\n"+"composion"+" "+comp)
 
 }*/
-fun main(){
+//________1_________
+/*fun main(){
+
+
   println("Введите цену которую заплатили за конфеты")
   val price: Double=readLine()?.toDouble()?:0.0
 
@@ -36,4 +40,110 @@ fun main(){
 
   println("И ксатит мои конфекты стоят "+pricebykg+" За килограмм")
 
+}*/
+//_______2_______
+/*fun main() {
+  println("Введите первое чиило")
+  val num1: Double = readln()?.toDouble() ?: 0.0
+
+  println("Введите первое чиило")
+  val num2: Double = readln()?.toDouble() ?: 0.0
+
+  println("Введите первое чиило")
+  val num3: Double = readln()?.toDouble() ?: 0.0
+
+  if(num1==num2||num1==num3||num2==num3){
+    println("yes")
+  }
+  else{
+    println("no")
+  }
+
+}*/
+
+//_________3_________
+/*fun main(){
+  for(i in 1..2){
+    for(j in 1..5){
+      print("${i * j} \t")
+    }
+    println()
+  }
 }
+*/
+
+ //________4________
+/*fun main(){
+   var num:Int=(10..100).random()
+  var mainarray = IntArray(num)
+   println("Индекс массива = "+mainarray.size)
+   for(i in 0 ..num-1){
+
+        mainarray[i]=(10..100).random()
+     println(mainarray[i])
+
+   }
+
+
+   println("Max by array = "+ mainarray.maxOrNull()+"\n"+"Min by array = "+ mainarray.minOrNull())
+}
+*/
+
+//__________5_________
+
+public abstract class figure(){
+
+  abstract fun num_corners():Int
+  abstract fun perimeter():Int
+  abstract fun area():Int
+  }
+ public class rectangle():figure() {
+
+    public var width:Int
+      get(){return  width}
+      set(value){
+        width=value
+      }
+   public var height:Int
+     get(){return  height}
+     set(value){
+       height=value
+     }
+
+   override fun perimeter(): Int {
+     return width * 2 + height * 2;
+   }
+
+   override fun area(): Int {
+     return width * height;
+
+   }
+   override  fun num_corners():Int{
+     return 4;
+   }
+ }
+class circle():figure() {
+  public var radius: Int
+    get() {
+      return radius
+    }
+    set(value) {
+      radius = value
+    }
+
+
+  override fun perimeter():Int{
+
+    return (PI * 2 * radius).toInt()
+  }
+
+  override fun area(): Int {
+    return ((PI*PI)*radius).toInt()
+
+  }
+
+  override fun num_corners(): Int {
+    return 0;
+  }
+}
+
