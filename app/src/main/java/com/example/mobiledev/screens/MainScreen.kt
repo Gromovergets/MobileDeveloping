e4ed07aca573ba9e1555d8eacc2767712666a3b3
Gromovergets <daduuuuu79@gmail.com>
2024-11-07 23:57:11 +0300

    Прогноз погоды 6, На этом уроке по Jetpack compose создаем data class WeatherModel.

--- app/src/main/java/com/example/mobiledev/screens/MainScreen.kt
+++ app/src/main/java/com/example/mobiledev/screens/MainScreen.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
@@ -35,6 +37,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.mobiledev.R
+import com.example.mobiledev.data.WeatherModel
 import com.example.mobiledev.ui.theme.BlueLight
 import com.google.accompanist.pager.ExperimentalPagerApi
 import kotlinx.coroutines.launch
@@ -141,7 +144,7 @@ fun MainCard() {
 }
 
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalPagerApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun TabLayout() {
     val tabList = listOf("HOURS", "DAYS")
@@ -189,9 +192,32 @@ fun TabLayout() {
         ) { index ->
             LazyColumn (
                 modifier = Modifier.fillMaxSize()
-            ){
-                items(15){
-                    ListItem()
+            ) {
+                itemsIndexed(
+                    listOf(
+                        WeatherModel(
+                            "London",
+                            "10:00",
+                            "25°C",
+                            "Sunny",
+                            "//cdn.weatherapi.com/weather/64x64/day/176.png",
+                            "",
+                            "",
+                            ""
+                        ),
+                        WeatherModel(
+                            "London",
+                            "26-07-2022",
+                            "",
+                            "Sunny",
+                            "//cdn.weatherapi.com/weather/64x64/day/176.png",
+                            "26°C",
+                            "12°C",
+                            "info"
+                        )
+                    )
+                ) {
+                        _, item -> ListItem(item)
                 }
             }
         }
