113ca0e347178972ff122e651491bf295c76fad8
Gromovergets <daduuuuu79@gmail.com>
2024-11-08 03:50:52 +0300

    Справочник ботаника 4. На этом уроке добавляем Navigation, создаем InfoScreen для показа дополнительной информации выбранного элемента в WebView с помощью файла HTML .

--- app/src/main/java/com/example/mobiledev/MainActivity.kt
+++ app/src/main/java/com/example/mobiledev/MainActivity.kt
@@ -1,87 +1,42 @@
 package com.example.mobiledev
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.mobiledev.ui.theme.MobileDevTheme
-import com.example.mobiledev.ui_components.DrawerMenu
-import com.example.mobiledev.ui_components.MainListItem
-import com.example.mobiledev.ui_components.MainTopBar
-import com.example.mobiledev.utils.DrawerEvents
-import com.example.mobiledev.utils.IdArrayList
+import com.example.mobiledev.ui_components.InfoScreen
+import com.example.mobiledev.ui_components.MainScreen
 import com.example.mobiledev.utils.ListItem
-import kotlinx.coroutines.launch
+import com.example.mobiledev.utils.Routes
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scaffoldState = rememberScaffoldState()
-            val coroutineScope = rememberCoroutineScope()
-            val mainList = remember {
-                mutableStateOf(getListItemsByIndex(0, this))
-            }
-            val topBarTitle = remember {
-                mutableStateOf("Грибы")
-            }
+            val navController = rememberNavController()
+            var item: ListItem? = null
             MobileDevTheme {
-                Scaffold(
-                    scaffoldState = scaffoldState,
-                    topBar = {
-                        MainTopBar(
-                            title = topBarTitle.value,
-                            scaffoldState
-                        )
-                    },
-                    drawerContent = {
-                        DrawerMenu{ event ->
-                            when(event){
-                                is DrawerEvents.OnItemClick ->{
-                                    topBarTitle.value = event.title
-                                    mainList.value = getListItemsByIndex(
-                                        event.index, this@MainActivity)
-                                }
-                            }
-                            coroutineScope.launch {
-                                scaffoldState.drawerState.close()
-                            }
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.MAIN_SCREEN
+                ){
+                    composable(Routes.MAIN_SCREEN){
+                        MainScreen(context = this@MainActivity){ listItem ->
+                            item = listItem
+                            navController.navigate(Routes.INFO_SCREEN)
                         }
                     }
-                ) {
-                    LazyColumn(modifier = Modifier.fillMaxSize()){
-                        items(mainList.value){ item ->
-                            MainListItem(item = item)
+                    composable(Routes.INFO_SCREEN){
+                        InfoScreen(item = item!!)
                         }
                     }
                 }
             }
         }
     }
-}
 
-private fun getListItemsByIndex(index: Int, context: Context): List<ListItem>{
-    val list = ArrayList<ListItem>()
-    val arrayList = context.resources.getStringArray(IdArrayList.listId[index])
-    arrayList.forEach { item ->
-        val itemArray = item.split("|")
-        list.add(
-            ListItem(
-                itemArray[0],
-                itemArray[1]
-            )
-        )
-    }
-    return list
-}
+
+
