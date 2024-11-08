963c44356236e9f63b05ba4392a5dc56b2f43438
Gromovergets <daduuuuu79@gmail.com>
2024-11-08 03:31:51 +0300

    Справочник ботаника 3. На этом уроке создаем разметку для показа основного списка с выбранной категории а также добавляем функцию для получения списка выбранной категории.

--- app/src/main/java/com/example/mobiledev/MainActivity.kt
+++ app/src/main/java/com/example/mobiledev/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.mobiledev
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import com.example.mobiledev.ui.theme.MobileDevTheme
 import com.example.mobiledev.ui_components.DrawerMenu
+import com.example.mobiledev.ui_components.MainListItem
 import com.example.mobiledev.ui_components.MainTopBar
 import com.example.mobiledev.utils.DrawerEvents
+import com.example.mobiledev.utils.IdArrayList
+import com.example.mobiledev.utils.ListItem
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +30,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             val scaffoldState = rememberScaffoldState()
             val coroutineScope = rememberCoroutineScope()
+            val mainList = remember {
+                mutableStateOf(getListItemsByIndex(0, this))
+            }
             val topBarTitle = remember {
                 mutableStateOf("Грибы")
             }
@@ -39,6 +50,8 @@ class MainActivity : ComponentActivity() {
                             when(event){
                                 is DrawerEvents.OnItemClick ->{
                                     topBarTitle.value = event.title
+                                    mainList.value = getListItemsByIndex(
+                                        event.index, this@MainActivity)
                                 }
                             }
                             coroutineScope.launch {
@@ -47,9 +60,28 @@ class MainActivity : ComponentActivity() {
                         }
                     }
                 ) {
-
+                    LazyColumn(modifier = Modifier.fillMaxSize()){
+                        items(mainList.value){ item ->
+                            MainListItem(item = item)
+                        }
+                    }
                 }
             }
         }
     }
+}
+
+private fun getListItemsByIndex(index: Int, context: Context): List<ListItem>{
+    val list = ArrayList<ListItem>()
+    val arrayList = context.resources.getStringArray(IdArrayList.listId[index])
+    arrayList.forEach { item ->
+        val itemArray = item.split("|")
+        list.add(
+            ListItem(
+                itemArray[0],
+                itemArray[1]
+            )
+        )
+    }
+    return list
 }
---
+++ app/src/main/java/com/example/mobiledev/utils/IdArrayList.kt
@@ -0,0 +1,13 @@
+package com.example.mobiledev.utils
+
+import com.example.mobiledev.R
+
+object IdArrayList {
+    val listId = listOf(
+        R.array.mushrooms,
+        R.array.medicinal_herbs,
+        R.array.trees,
+        R.array.fruits,
+        R.array.vegetables
+    )
+}
